ce87b10642ede200276bd7d8e573d7d33989cac9
Tsotne Kekelia <cotne.kekelia@yahoo.com>
2020-11-02 15:12:37 +0200

    Initial Commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,146 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div class="hello">
+        <div>
+            <ul>
+                <li>Firstname: {{user.name | capitalize}}</li>
+                <br>
+                <li>Lastname: {{user.lastname | capitalize}}</li>
+                <br>
+                <li>Birth date: {{user.birthdate | formatDate | capitalize}}</li>
+                <br>
+                <li>Age: {{calculatedAge}}</li>
+                <br>
+                <li>Gender: {{user.gender}}</li>
+                <br>
+                <li style="white-space: pre-line;">About me: {{user.aboutMe}}</li>
+                <br>
+            </ul>
+        </div>
+        <div>
+            <input type="text" v-model="user.name" placeholder="Firstname">
+        </div>
+        <div>
+            <input type="text" v-model="user.lastname" placeholder="Lastname">
+        </div>
+        <div>
+            <input type="date" v-model="user.birthdate" placeholder="Birth date">
+        </div>
+        <div>
+            <input type="radio" v-model="user.gender" value="male" id="male"> <label for="male">Male</label>
+            <br>
+            <input type="radio" v-model="user.gender" value="female" id="female"> <label for="female">Female</label>
+        </div>
+        <div>
+            <textarea v-model="user.aboutMe" placeholder="About me..."></textarea>
+        </div>
+        <button @click="submit">Submit</button>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+    import capitalize from '../Filters.js'
+
+    export default {
+        name: 'HelloWorld',
+        data: function () {
+            return {
+                user: {
+                    name: null,
+                    lastname: null,
+                    birthdate:  null,
+                    age: null,
+                    gender: null,
+                    aboutMe: null
+                }
+            }
+        },
+        computed: {
+            calculatedAge: function () {
+                console.log('I am running')
+                if (!this.user.birthdate) return ''
+                let birthDate = new Date(this.user.birthdate);
+                let currentDate = new Date();
+
+                return currentDate.getFullYear() - birthDate.getFullYear()
+            }
+        },
+        methods: {
+            submit: function () {
+                console.log(this.user)
+                // send data to api
+            }
+        },
+        filters: {
+            capitalize: capitalize,
+            formatDate: function (value) {
+                if (!value) return ''
+                let date = new Date(value);
+                let month = date.getMonth();
+                let day = date.getDate();
+                let year = date.getFullYear();
+
+                let months = [
+                    'january',
+                    'february',
+                    'march',
+                    'april',
+                    'may',
+                    'june',
+                    'july',
+                    'august',
+                    'september',
+                    'october',
+                    'november',
+                    'december'
+                ]
+
+                let m = months[month];
+                let d =  '';
+                if (day == 11 || day == 12 || day == 13) {
+                    d = day + 'th'
+                }  else {
+                    switch (day % 10) {
+                        case 1:
+                            d = day + 'st'
+                            break;
+                        case 2:
+                            d = day + 'nd'
+                            break;
+                        case 3:
+                            d = day + 'rd'
+                            break;
+                        default:
+                            d = day + 'th'
+                    }
+                }
+
+                return m + " " + d + ", " + year
+
+            }
+        },
+        props: {
+            msg: String
+        }
+    }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+    h3 {
+        margin: 40px 0 0;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
 </style>
